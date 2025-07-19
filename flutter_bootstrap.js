66528c8a18651d6eced2052fc2b530b3c659ae80
Salim1202 <salimlive@ymail.com>
2025-07-20 00:09:45 +0530

    bulk and pending order changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4198673296"
+    serviceWorkerVersion: "16467965"
   }
 });
